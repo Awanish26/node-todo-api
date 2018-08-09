6d995ab173a91b9512339eb0bb7c56c7e39445dc
Awanish Sinha <awanish.sinha26@gmail.com>
2018-08-09 06:36:19 +0530

    add POST/todos route and refactot mongoose

--- server/server.js
+++ server/server.js
@@ -13,7 +13,7 @@ app.post('/todos',(req,res)=>{
 var todo=new Todo({
     text:req.body.text
 });
-todo.save().then((doc)=>{
+todo.save().then((doc)=>{               //saves the model to the databasee
 res.send(doc);
 },(e)=>{
     res.status(400).send(e);
@@ -22,4 +22,4 @@ res.send(doc);
 
 app.listen(3000,()=>{
     console.log('Started on port 3000');
-})
+});
